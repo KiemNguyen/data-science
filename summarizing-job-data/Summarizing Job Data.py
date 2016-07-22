c67f20166319a29896fee433ce924dbaca9a6ad4
Kiem Nguyen <kiemnguyen@gmail.com>
2016-07-22 15:40:16 -0500

    Initial commit

--- summarizing-job-data/Summarizing Job Data.py
+++ summarizing-job-data/Summarizing Job Data.py
@@ -32,20 +32,3 @@ recent_grads_sum = recent_grads['Total'].sum()
 
 low_wage_percent = low_wage_jobs_sum / recent_grads_sum
 print(low_wage_percent)
-
-## 5. Comparing Data Sets ##
-
-# All majors, common to both DataFrames
-majors = recent_grads['Major'].unique()
-rg_lower_count = 0
-for major in majors:
-    recent_grads_row = recent_grads[recent_grads["Major"] == major]
-    all_ages_row = all_ages[all_ages["Major"] == major]
-    
-    rg_unemp_rate = recent_grads_row.iloc[0]["Unemployment_rate"]
-    aa_unemp_rate = all_ages_row.iloc[0]["Unemployment_rate"]
-    
-    if rg_unemp_rate < aa_unemp_rate:
-        rg_lower_count += 1
-        
-print(rg_lower_count )

094ac98e2a8da136e8478a20f79695011b31324a
Kiem Nguyen <kiemnguyen@gmail.com>
2016-07-31 22:28:35 -0500

    Added custom index for selection

--- data-analysis-and-visualization /Pandas Dataframes.py
+++ data-analysis-and-visualization /Pandas Dataframes.py
@@ -19,3 +19,8 @@ print(first_last)
 fandango = pd.read_csv('fandango_score_comparison.csv')
 fandango_films = fandango.set_index("FILM", drop=False)
 print(fandango_films.index)
+
+## 4. Using a Custom Index for Selection ##
+
+movies = ["The Lazarus Effect (2015)", "Gett: The Trial of Viviane Amsalem (2015)", "Mr. Holmes (2015)"]
+best_movies_ever = fandango_films.loc[movies]

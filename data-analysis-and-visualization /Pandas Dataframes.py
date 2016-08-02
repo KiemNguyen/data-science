a3c06206500a3c28eef88defdae417be5347228c
Kiem Nguyen <kiemnguyen@gmail.com>
2016-08-01 22:46:40 -0500

    Applied logic over columns and rows

--- data-analysis-and-visualization /Pandas Dataframes.py
+++ data-analysis-and-visualization /Pandas Dataframes.py
@@ -24,3 +24,20 @@ print(fandango_films.index)
 
 movies = ["The Lazarus Effect (2015)", "Gett: The Trial of Viviane Amsalem (2015)", "Mr. Holmes (2015)"]
 best_movies_ever = fandango_films.loc[movies]
+
+## 6. Apply() Logic Over Columns: Practice ##
+
+double_df = float_df.apply(lambda x: x*2)
+print(double_df.head(1))
+
+halved_df = float_df.apply(lambda x: x/2)
+print(halved_df.head(1))
+
+## 7. Apply() Over Dataframe Rows ##
+
+rt_mt_user = float_df[['RT_user_norm', 'Metacritic_user_nom']]
+rt_mt_deviations = rt_mt_user.apply(lambda x: np.std(x), axis=1)
+print(rt_mt_deviations[0:5])
+
+rt_mt_means = rt_mt_user.apply(lambda x: np.mean(x), axis=1)
+print(rt_mt_means[0:5])
